cd4b4d7db6b3a424f8590185681de1ccb75cb946
Mikrolous <pernillelous@gmail.com>
2015-11-11 11:41:15 +0100

    TCPListener and TCPClient added

--- MiniProject3/MiniProject3/PutClient.cs
+++ MiniProject3/MiniProject3/PutClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,40 @@ namespace MiniProject3
 {
     public class PutClient
     {
+        public PutClient()
+        {
+            //c
+        }
+
+        public void PutRequest(int key, string value)
+        {
+
+        }
+
+        public void TcpListener()
+        {
+            TcpListener server = null;
+            try
+            {
+                int port = 8080;
+                IpAdress local
+            }
+        }
+
+
+        static void Connect(string ip, int port)
+        {
+            try
+            {
+                port = 8080;
+                TcpClient client = new TcpClient( ip, port);
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                
+            }
+
+        }
+
     }
 }
